cca4078f1498590df14ac425f7bbf0766bd26399
Gogu Paul <68021113+gogupaul123@users.noreply.github.com>
2021-03-26 19:53:05 +0200

    caca

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ const img4 = document.querySelector('.img4')
  
 
 grid1.addEventListener('mouseenter',()=>{
-    console.log('grid1 enter')
+ 
     text1.classList.add('display')
     img1.classList.add('opacity'); 
     img1.classList.add('scale')
 })
 grid1.addEventListener('mouseleave',()=>{
-    console.log('grid1 leave')
+     
     text1.classList.remove('display')
     img1.classList.remove('opacity'); 
     img1.classList.remove('scale') 
@@ -31,13 +31,13 @@ grid1.addEventListener('mouseleave',()=>{
 
 
 grid2.addEventListener('mouseenter',()=>{
-    console.log('grid1 enter')
+   
     text2.classList.add('display')
     img2.classList.add('opacity'); 
     img2.classList.add('scale')
 })
 grid2.addEventListener('mouseleave',()=>{
-    console.log('grid1 leave')
+     
     text2.classList.remove('display')
     img2.classList.remove('opacity'); 
     img2.classList.remove('scale') 
@@ -46,13 +46,13 @@ grid2.addEventListener('mouseleave',()=>{
 
 
 grid3.addEventListener('mouseenter',()=>{
-    console.log('grid1 enter')
+     
     text3.classList.add('display')
     img3.classList.add('opacity'); 
     img3.classList.add('scale')
 })
 grid3.addEventListener('mouseleave',()=>{
-    console.log('grid1 leave')
+ 
     text3.classList.remove('display')
     img3.classList.remove('opacity'); 
     img3.classList.remove('scale') 
@@ -60,13 +60,13 @@ grid3.addEventListener('mouseleave',()=>{
 
 
 grid4.addEventListener('mouseenter',()=>{
-    console.log('grid1 enter')
+   
     text4.classList.add('display')
     img4.classList.add('opacity'); 
     img4.classList.add('scale')
 })
 grid4.addEventListener('mouseleave',()=>{
-    console.log('grid1 leave')
+    
     text4.classList.remove('display')
     img4.classList.remove('opacity'); 
     img4.classList.remove('scale') 
